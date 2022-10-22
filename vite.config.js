b31a81f7eaaec891fc5faf976f60c39242576cb0
Your Name <you@example.com>
2022-10-22 16:37:35 +0200

    change

--- vite.config.js
+++ vite.config.js
@@ -13,11 +13,26 @@ export default defineConfig({
     outDir: resolve("dist"),
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
-        main: resolve(__dirname, "src/about.html"),
-        main: resolve(__dirname, "src/project.html"),
-        main: resolve(__dirname, "src/insight.html"),
-        main: resolve(__dirname, "src/contact.html"),
+        main: resolve(
+          __dirname,
+          "src/index.html"
+        ),
+        about: resolve(
+          __dirname,
+          "src/about.html"
+        ),
+        project: resolve(
+          __dirname,
+          "src/project.html"
+        ),
+        insight: resolve(
+          __dirname,
+          "src/insight.html"
+        ),
+        contact: resolve(
+          __dirname,
+          "src/contact.html"
+        ),
 
         // about: resolve(__dirname, "src/about.html"),
       },
